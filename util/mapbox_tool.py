eaf42f5c8d82357759e48ff10e361113b91a6778
listenzcc <cc.public@outlook.com>
2023-11-27 14:03:00 +0800

    Fix incorrect line-break

--- util/mapbox_tool.py
+++ util/mapbox_tool.py
@@ -39,8 +39,7 @@ def fetch_png(lon, lat, img_width, img_height, access_token=None):
     assert access_token is not None, "Invalid access_token"
     box = f'{mk_box(lon, lat)}'
     size = f'{img_width:d}x{img_height:d}'
-    url = f'https://wrong.api.mapbox.com/styles/v1/mapbox/streets-v12/static/{
-        box}/{size}?access_token={access_token}'
+    url = f'https://wrong.api.mapbox.com/styles/v1/mapbox/streets-v12/static/{box}/{size}?access_token={access_token}'
     resp = requests.get(url)
     # print(url, resp)
     io = BytesIO(resp.content)
